2deef5af0092d3002dde1c25dc7567a078c947ca
Ali Ghaffari <aligh71@yahoo.com>
2024-03-08 19:17:32 +0330

    the first stage form and sending the mobile number to the back-end

---
+++ src/components/templates/SendOtpForm.jsx
@@ -0,0 +1,38 @@
+import toast, { Toaster } from "react-hot-toast";
+import { sendOtp } from "../../services/auth";
+
+function SendOtpForm({ mobile, setMobile, setStep }) {
+  const submitHandler = async (event) => {
+    event.preventDefault();
+    if (mobile.length !== 11)
+      return toast.error("لطفا شماره موبایل را درست وارد نمایید!");
+    const { response, error } = await sendOtp(mobile);
+
+    if (response) setStep(2);
+    if (error)
+      return toast.error("خطایی رخ داده است!");
+  };
+  return (
+    <>
+      <Toaster />
+      <form onSubmit={submitHandler}>
+        <p>ورود به حساب کاربری</p>
+        <span>
+          برای استفاده از امکانات دیوار، لطفا شماره موبایل خود را وارد کنید. کد
+          تایید به این شماره پیامک خواهد شد.
+        </span>
+        <label htmlFor="input">شماره موبایل خود را وارد کنید</label>
+        <input
+          type="text"
+          id="input"
+          placeholder="شماره موبایل"
+          value={mobile}
+          onChange={(e) => setMobile(e.target.value)}
+        />
+        <button type="submit">ارسال کد تایید</button>
+      </form>
+    </>
+  );
+}
+
+export default SendOtpForm;
