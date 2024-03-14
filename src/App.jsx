c72d3134a227eb5d41dea0cdd8ff85cdd7aefac8
Ali Ghaffari <aligh71@yahoo.com>
2024-03-14 20:31:04 +0330

    add layout

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,16 @@ import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import reactQueryConfigs from "configs/reactQuery";
+import Layout from "layouts/Layout";
 
 function App() {
   const queryClient = new QueryClient(reactQueryConfigs);
   return (
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
-        <Router />
+        <Layout>
+          <Router />
+        </Layout>
       </BrowserRouter>
       <ReactQueryDevtools />
     </QueryClientProvider>
---
+++ src/layouts/Footer.jsx
@@ -0,0 +1,10 @@
+import styles from "./Footer.module.css";
+function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <p>Developed by Ali with 💖</p>
+    </footer>
+  );
+}
+
+export default Footer;
