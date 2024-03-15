9480a9b5cefe29c5aac73576cb8d9f0c572d0b19
Ali Ghaffari <aligh71@yahoo.com>
2024-03-15 15:50:42 +0330

    convert date and numbers to Farsi and add delete button to posts

--- src/components/templates/PostList.jsx
+++ src/components/templates/PostList.jsx
@@ -1,28 +1,37 @@
-import { useQuery } from "@tanstack/react-query";
-import { getPosts } from "services/user";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { deletePost, getPosts } from "services/user";
 import Loader from "../modules/Loader";
+import { sp } from "utils/replaceNumber";
+import styles from "./PostList.module.css";
 
 function PostList() {
+  const queryClient = useQueryClient();
+
   const { data, isLoading } = useQuery(["my-post-list"], getPosts);
-  console.log(data);
+
+  const { mutate } = useMutation(deletePost, {
+    onSuccess: () => queryClient.invalidateQueries("my-post-list"),
+  });
+
   return (
-    <div>
+    <div className={styles.list}>
       {isLoading ? (
         <Loader />
       ) : (
         <>
           <h3>آگهی های شما</h3>
           {data.data.posts.map((post) => (
-            <div key={post._id}>
+            <div key={post._id} className={styles.post}>
               <img src={`${import.meta.env.VITE_BASE_URL}${post.images[0]}`} />
               <div>
                 <p>{post.options.title}</p>
                 <span>{post.options.content}</span>
               </div>
-              <div>
-                <p>{post.createdAt}</p>
-                <span>{post.amount} تومان</span>
+              <div className={styles.price}>
+                <p>{new Date(post.createdAt).toLocaleDateString("fa-IR")}</p>
+                <span>{sp(post.amount)} تومان</span>
               </div>
+              <button onClick={() => mutate(post._id)}>حذف آگهی</button>
             </div>
           ))}
         </>
--- src/services/user.js
+++ src/services/user.js
@@ -4,4 +4,6 @@ const getProfile = () => api.get("user/whoami").then((res) => res || false);
 
 const getPosts = () => api.get("post/my");
 
-export { getProfile,getPosts };
+const deletePost = (id) => api.delete(`post/delete/${id}`);
+
+export { getProfile, getPosts, deletePost };
